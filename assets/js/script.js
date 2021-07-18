d7e05301aaec6efad2b0abad2bbf8201bb3322d3
zacktmiller92 <zacktmiller92@gmail.com>
2021-07-18 13:17:36 -0400

    created calBackgroundColor function to change background of calendar depending on the time

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,24 @@ var calEnter = $("#cal-enter");
 $("#currentDay").text(moment().format("dddd, MMM Do YYYY"))
 
 
+// color code blocks based on date
+function calBackgroundColor () {
+    $("#cal-list li").each(function(index){
 
+        if (index + 9 < moment().hour()) {
+            $(this).find("#cal-enter").addClass("past");
+        }
+        else if (index + 9 == moment().hour()) {
+            $(this).find("#cal-enter").addClass("present");
+        }
+        else {
+            $(this).find("#cal-enter").addClass("future");
+        }
 
+    })
+    // console.log('calBackgroundColor has been called')
+};
+
+calBackgroundColor()
+setInterval(calBackgroundColor, (1000 * 60 * 5));
 
